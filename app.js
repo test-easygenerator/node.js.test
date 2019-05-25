10bee865b7085bbbf416471b0d1e9a590824e8dc
P.Talko <golodnaya@hochet.est>
2019-05-25 23:45:06 +0300

    Upload Node.js application

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const APP_PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('Hello World\n');
+});
+
+server.listen(APP_PORT, function () {
+    console.log(`Server running on port ${this.address().port}`);
+});
